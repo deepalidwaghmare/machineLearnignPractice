b0e0a1b5d185b456883179799faa85eb26870052
Deepali <deepalidwaghmare@yahoo.com>
2023-06-29 11:36:46 +0100

    Day1

---
+++ main.py
@@ -0,0 +1,12 @@
+#Day 1
+
+#Indexing
+c = str("Hello World")
+print("+ve Indexing: 7", c[7])
+print("-ve Indexing: -5", c[-5])
+print("First 5 letters:", c[0:7])
+print("Last 5 letters:", c[4:])
+print("First 4 letters:", c[:4])
+print("Skip letters:", c[0:7:3])
+#Reverse String in Python
+print("Reverse String", c[::-1])
