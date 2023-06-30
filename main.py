cd73c85cb27711e4698e6f63900acb4179276c80
Deepali <deepalidwaghmare@yahoo.com>
2023-06-30 12:41:26 +0100

    Day2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#Day 1
+# Day 1
 
-#Indexing
+# Indexing and String Operators
 c = str("Hello World")
 print("+ve Indexing: 7", c[7])
 print("-ve Indexing: -5", c[-5])
@@ -8,5 +8,10 @@ print("First 5 letters:", c[0:7])
 print("Last 5 letters:", c[4:])
 print("First 4 letters:", c[:4])
 print("Skip letters:", c[0:7:3])
-#Reverse String in Python
+# Reverse String in Python
 print("Reverse String", c[::-1])
+# Str is immutable but del c can be used
+print(c * 4)
+
+# lexicographical comparison in alphabets a<b A<a
+print("kol"<"Kol")
