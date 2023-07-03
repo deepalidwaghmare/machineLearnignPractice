b40d2f7bd4e19bb9e7f8ed94e7c85cbf95c37981
Deepali <deepalidwaghmare@yahoo.com>
2023-07-03 12:12:20 +0100

    Day3

--- main.py
+++ main.py
@@ -14,4 +14,22 @@ print("Reverse String", c[::-1])
 print(c * 4)
 
 # lexicographical comparison in alphabets a<b A<a
-print("kol"<"Kol")
+print("kol" < "Kol")
+print("hello" and "world")
+print("" and "Hello")  # Empty String = false and Non-Empty String = true
+print("" or "Hello")
+
+# Tricky AND OR
+print("Hello" or "World")  # Hello: 1 or 0  will be 1 no matter what is next
+print("Hello" and "World")  # World: 1 and 0 = 0,,,1 and 1 = 1 need to confirm the second input
+
+# Slicing in Loop
+for i in c[2:9:2]:
+    print(i)
+for i in c[::-1]:
+    print(i)
+
+# Membership operator IN notIn
+print('h' in c or 'H' in c)  # False or True = True
+print('world' not in c and 'World' not in c)   # True and False = False
+
